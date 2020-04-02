78037a2e672bc75a97ecf1a7f3b7ec76ddd93981
Tal Miller <talmiller@gmail.com>
2020-04-02 15:39:47 +0300

    add parametric run/plot scripts

---
+++ plot_parametric.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from default_settings import define_default_settings
+from relaxation_algorithm_functions import load_simulation
+
+# plt.close('all')
+
+# parametric scan
+# save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_vth_right_bc_uniform_scaling_transition_n_factor_0.1_delta_n_factor_0.01/'
+# save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_vth_right_bc_uniform_scaling_transition_n_factor_0.1_delta_n_factor_0.1/'
+# save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_vth_right_bc_uniform_scaling_transition_n_factor_0.5_delta_n_factor_0.1/'
+# save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_vth_right_bc_uniform_scaling_transition_n_factor_0.01_delta_n_factor_0.01/'
+# save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_vth_right_bc_uniform_scaling_transition_n_factor_0.1_delta_n_factor_0.1_DT_mix/'
+
+
+# Rm_list = np.array([1.4, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0])
+# Rm_list = np.array([2.0, 2.5, 3.0])
+# Rm_list = np.array([2.5, 3.0])
+Rm_list = np.array([3.0])
+U0_list = np.array([0, 1e4, 1e5, 2e5, 3e5, 4e5, 5e5, 6e5, 7e5, 8e5, 9e5, 1e6])
+
+for Rm in Rm_list:
+    # print('Rm=' + str(Rm))
+    flux_list = 0 * U0_list
+    L_stable_list = 0 * U0_list
+    n_stable_list = 0 * U0_list
+
+    for i, U0 in enumerate(U0_list):
+        # print('Rm=' + str(Rm) + ', U0=' + str(U0))
+
+        save_dir = save_dir_main + '/Rm_' + str(Rm) + '_U_' + '{:.1e}'.format(U0)
+
+        state_file = save_dir + '/state.pickle'
+        settings_file = save_dir + '/settings.pickle'
+        state, settings = load_simulation(state_file, settings_file)
+
+        flux_list[i] = state['flux_mean']
+
+        # if state['termination_criterion_reached'] is True:
+        #     flux_list[i] = state['flux_mean']
+        # else:
+        #     print('failed: Rm=' + str(Rm) + ', U0=' + str(U0))
+        #     flux_list[i] = np.nan
+
+        # print(flux_list)
+
+        ind_n_stable = np.where(state['n'] < settings['n_end'] * 1.01)[0][0]
+        n_stable_list[i] = state['n'][ind_n_stable]
+        z_array = np.cumsum(state['mirror_cell_sizes'])
+        L_stable_list[i] = z_array[ind_n_stable]
+
+    plt.figure(1)
+    plt.plot(U0_list, flux_list, '-o', label='Rm=' + str(Rm))
+
+    plt.figure(2)
+    plt.plot(U0_list, L_stable_list, '-o', label='Rm=' + str(Rm))
+
+    plt.figure(3)
+    plt.plot(U0_list, n_stable_list, '-o', label='Rm=' + str(Rm))
+
+
+plt.figure(1)
+plt.xlabel('$U_0$ [m/s]')
+plt.ylabel('flux')
+plt.tight_layout()
+plt.grid(True)
+plt.legend()
+
+plt.figure(2)
+plt.xlabel('$U_0$ [m/s]')
+plt.ylabel('$L_{stable}$')
+plt.tight_layout()
+plt.grid(True)
+plt.legend()
+
+plt.figure(3)
+plt.xlabel('$U_0$ [m/s]')
+plt.ylabel('$n_{stable}$')
+plt.tight_layout()
+plt.grid(True)
+plt.legend()
---
+++ run_parametric.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import os
+from default_settings import define_default_settings
+from relaxation_algorithm_functions import find_rate_equations_steady_state
+
+# parametric scan
+# save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_vth_right_bc_uniform_scaling_transition_n_factor_0.1_delta_n_factor_0.01/'
+# save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_vth_right_bc_uniform_scaling_transition_n_factor_0.1_delta_n_factor_0.1/'
+# save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_vth_right_bc_uniform_scaling_transition_n_factor_0.5_delta_n_factor_0.1/'
+# save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_vth_right_bc_uniform_scaling_transition_n_factor_0.01_delta_n_factor_0.01/'
+# save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_vth_right_bc_uniform_scaling_transition_n_factor_0.1_delta_n_factor_0.1_DT_mix/'
+save_dir_main = 'runs/runs_smooth_transition_adjust_cell_size_mfp_right_bc_uniform_scaling_transition_n_factor_0.1_delta_n_factor_0.1/'
+
+if not os.path.exists(save_dir_main):
+    os.mkdir(save_dir_main)
+
+
+# for Rm in [1.4, 2.0, 3.0]:
+# for Rm in [2.0]:
+# for Rm in [2.5]:
+for Rm in [3.0]:
+# for Rm in [3.5, 4.0, 4.5, 5.0]:
+    # for U0 in [0, 1e4, 1e5, 2e5, 3e5, 4e5, 5e5, 6e5]:
+    # for U0 in [7e5, 8e5, 9e5, 1e6]:
+    # for U0 in [0, 1e4, 1e5, 2e5, 3e5, 4e5, 5e5, 6e5, 7e5, 8e5, 9e5, 1e6]:
+    for U0 in [1e4, 1e5, 2e5, 3e5, 4e5, 5e5, 6e5, 7e5, 8e5, 9e5, 1e6]:
+    # for U0 in [0]:
+    # for U0 in [1e4, 1e5]:
+    # for U0 in [2e5, 3e5, 4e5, 5e5, 6e5]:
+    # for U0 in [6e5, 7e5]:
+    # for U0 in [8e5, 9e5, 1e6]:
+
+        print('Rm=' + str(Rm) + ', U0=' + str(U0))
+
+        settings = {'Rm': Rm, 'U0': U0}
+        settings['save_dir'] = save_dir_main + '/Rm_' + str(settings['Rm']) + '_U_' + '{:.1e}'.format(settings['U0'])
+
+        if U0 == 0:
+            settings['number_of_cells'] = 300
+        elif U0 < 1e5:
+            settings['number_of_cells'] = 100
+        else:
+            settings['number_of_cells'] = 50
+
+        settings['transition_density_factor'] = 0.1
+        # settings['transition_density_factor'] = 0.5
+        # settings['transition_density_factor'] = 0.01
+
+        # settings['delta_n_smoothing_factor'] = 0.01
+        # settings['delta_n_smoothing_factor'] = 0.05
+        settings['delta_n_smoothing_factor'] = 0.1
+
+        settings['gas_name'] = 'hydrogen'
+        # settings['gas_name'] = 'DT_mix'
+
+        # settings['adaptive_mirror'] = 'adjust_cell_size_with_vth'
+        settings['adaptive_mirror'] = 'adjust_cell_size_with_mfp'
+
+        if settings['adaptive_mirror'] == 'adjust_cell_size_with_mfp':
+            settings['number_of_cells'] = 2 * settings['number_of_cells']
+
+        plt.close('all')
+
+        settings = define_default_settings(settings)
+        state = find_rate_equations_steady_state(settings)
+
