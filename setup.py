764a8e54bc580f2e579cc9d0585f443d980f8301
Tal Miller <talmiller@gmail.com>
2020-09-01 08:03:44 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="plasma-multi-mirror",
+    author="Tal Miller",
+    author_email="talmiller@gmail.com",
+    packages=['plasma-multi-mirror'],
+)
+
+# install locally using: python setup.py install --user
