456d3b26d71523439f5aca7bddf4f97967d77455
Tal Miller <talmiller@gmail.com>
2020-09-02 12:53:11 +0300

    draw_plots=False for slrum run fixed save_dir chdir to run in the correct dir so the logs appear there

--- run_scripts/run_parametric_slurm.py
+++ run_scripts/run_parametric_slurm.py
@@ -3,16 +3,22 @@ import os
 from mm_rate_eqs.default_settings import define_default_settings
 from mm_rate_eqs.slurm_functions import get_script_rate_eqs_slave
 
+pwd = os.getcwd()
+
 rate_eqs_script = get_script_rate_eqs_slave()
 
 settings = {}
 settings = define_default_settings(settings)
 
-settings['save_dir'] = '~/code/mm_rate_eqs/runs/slurm_runs/test/'
+settings['save_dir'] = '/home/talm/code/mm_rate_eqs/runs/slurm_runs/test/'
 print('save dir: ' + str(settings['save_dir']))
-
 os.makedirs(settings['save_dir'], exist_ok=True)
+os.chdir(settings['save_dir'])
+
+settings['draw_plots'] = False
 
 command = rate_eqs_script + ' --settings "' + str(settings) + '"'
 s = Slurm('job_name')
 s.run(command)
+
+os.chdir(pwd)
