f22dd13d180a9ab7e81aba7783a5697f5559b164
talmiller2 <talmiller@gmail.com>
2024-12-03 08:12:31 +0200

    upgraded the loop

--- run_scripts/run_slurm_parametric_N_RMF_from_mat.py
+++ run_scripts/run_slurm_parametric_N_RMF_from_mat.py
@@ -23,8 +23,8 @@ slurm_kwargs['cpus-per-task'] = 1
 
 plasma_mode = 'isoT'
 
-# num_cells_list = [10, 30, 50]
-num_cells_list = [30]
+num_cells_list = [10, 30, 50]
+# num_cells_list = [30]
 
 # load single_particle compiled mat
 # single_particle_dir = '/Users/talmiller/Downloads/single_particle/'
@@ -33,174 +33,204 @@ single_particle_dir += '/set53_B0_1T_l_1m_Post_Rm_10_intervals_D_T/'
 
 gas_name_list = ['deuterium', 'tritium']
 
-gas_name = 'deuterium'
-# gas_name = 'DT_mix'
-# gas_name = 'tritium'
+RF_type_list = []
+RF_amplitude_list = []
+induced_fields_factor_list = []
+with_kr_correction_list = []
 
-for gas_name in gas_name_list:
+RF_type_list += ['electric_transverse']
+RF_amplitude_list += [25]  # kV/m
+induced_fields_factor_list += [1]
+with_kr_correction_list += [True]
 
-    induced_fields_factor = 1
-    # induced_fields_factor = 0
-    time_step_tau_cyclotron_divisions = 50
-    # time_step_tau_cyclotron_divisions = 100
-    # sigma_r0 = 0
-    sigma_r0 = 0.05
-    # sigma_r0 = 0.1
-    radial_distribution = 'uniform'
+RF_type_list += ['electric_transverse']
+RF_amplitude_list += [50]  # kV/m
+induced_fields_factor_list += [1]
+with_kr_correction_list += [True]
 
-    # theta_type = 'sign_vz0'
-    theta_type = 'sign_vz'
+RF_type_list += ['magnetic_transverse']
+RF_amplitude_list += [0.02]  # T
+induced_fields_factor_list += [1]
+with_kr_correction_list += [True]
 
-    if gas_name == 'deuterium':
-        gas_name_short = 'D'
-    elif gas_name == 'tritium':
-        gas_name_short = 'T'
-    else:
-        gas_name_short = 'DTmix'
+RF_type_list += ['magnetic_transverse']
+RF_amplitude_list += [0.02]  # T
+induced_fields_factor_list += [0]
+with_kr_correction_list += [True]
 
-    # with_kr_correction = False
-    with_kr_correction = True
+RF_type_list += ['magnetic_transverse']
+RF_amplitude_list += [0.04]  # T
+induced_fields_factor_list += [1]
+with_kr_correction_list += [True]
 
-    RF_type = 'electric_transverse'
-    E_RF_kVm = 25  # kV/m
-    # E_RF_kVm = 50  # kV/m
-    # E_RF_kVm = 100  # kV/m
+RF_type_list += ['magnetic_transverse']
+RF_amplitude_list += [0.04]  # T
+induced_fields_factor_list += [0]
+with_kr_correction_list += [True]
 
-    # RF_type = 'magnetic_transverse'
-    # B_RF = 0.02  # T
-    # B_RF = 0.04  # T
-    # B_RF = 0.08  # T
+total_number_of_sets = len(RF_type_list) * len(gas_name_list)
+print('total_number_of_sets = ' + str(total_number_of_sets))
+cnt_sets = 0
 
-    set_name = 'compiled_'
-    set_name += theta_type + '_'
-    if RF_type == 'electric_transverse':
-        set_name += 'ERF_' + str(E_RF_kVm)
-    elif RF_type == 'magnetic_transverse':
-        set_name += 'BRF_' + str(B_RF)
-    if induced_fields_factor < 1.0:
-        set_name += '_iff' + str(induced_fields_factor)
-    if with_kr_correction == True:
-        set_name += '_withkrcor'
-    set_name += '_tcycdivs' + str(time_step_tau_cyclotron_divisions)
-    if sigma_r0 > 0:
-        set_name += '_sigmar' + str(sigma_r0)
-        if radial_distribution == 'normal':
-            set_name += 'norm'
-        elif radial_distribution == 'uniform':
-            set_name += 'unif'
-    set_name += '_' + gas_name
-    print(set_name)
-    single_particle_file = single_particle_dir + '/' + set_name + '.mat'
+for RF_type, RF_amplitude, induced_fields_factor, with_kr_correction \
+        in zip(RF_type_list, RF_amplitude_list, induced_fields_factor_list, with_kr_correction_list):
+    for gas_name in gas_name_list:
 
-    RF_rates_mat_dict = loadmat(single_particle_file)
-    alpha_loop_list = RF_rates_mat_dict['alpha_loop_list'][0]
-    beta_loop_list = RF_rates_mat_dict['beta_loop_list'][0]
-    N_rc_1 = RF_rates_mat_dict['N_rc']
-    N_lc_1 = RF_rates_mat_dict['N_lc']
-    N_cr_1 = RF_rates_mat_dict['N_cr']
-    N_cl_1 = RF_rates_mat_dict['N_cl']
-    N_rl_1 = RF_rates_mat_dict['N_rl']
-    N_lr_1 = RF_rates_mat_dict['N_lr']
+        cnt_sets += 1
+        print('@@@@@@@ set num', cnt_sets, '/', total_number_of_sets)
 
-    total_number_of_combinations = len(num_cells_list) * len(alpha_loop_list) * len(beta_loop_list)
-    print('total_number_of_combinations = ' + str(total_number_of_combinations))
-    cnt = 0
+        time_step_tau_cyclotron_divisions = 50
+        # time_step_tau_cyclotron_divisions = 100
+        # sigma_r0 = 0
+        sigma_r0 = 0.05
+        # sigma_r0 = 0.1
+        radial_distribution = 'uniform'
 
-    # create corresponding dir in the mm_rate_eqs
-    sub_folder = main_folder + '/' + set_name
-    print('creating sub_folder:', sub_folder)
-    os.makedirs(sub_folder, exist_ok=True)
+        # theta_type = 'sign_vz0'
+        theta_type = 'sign_vz'
 
-    for ind_beta, beta in enumerate(beta_loop_list):
-        for ind_alpha, alpha in enumerate(alpha_loop_list):
+        if gas_name == 'deuterium':
+            gas_name_short = 'D'
+        elif gas_name == 'tritium':
+            gas_name_short = 'T'
+        else:
+            gas_name_short = 'DTmix'
 
-            RF_rc_curr = RF_rates_mat_dict['N_rc'][ind_beta, ind_alpha]
-            RF_lc_curr = RF_rates_mat_dict['N_lc'][ind_beta, ind_alpha]
-            RF_cr_curr = RF_rates_mat_dict['N_cr'][ind_beta, ind_alpha]
-            RF_cl_curr = RF_rates_mat_dict['N_cl'][ind_beta, ind_alpha]
-            RF_rl_curr = RF_rates_mat_dict['N_rl'][ind_beta, ind_alpha]
-            RF_lr_curr = RF_rates_mat_dict['N_lr'][ind_beta, ind_alpha]
+        set_name = 'compiled_'
+        set_name += theta_type + '_'
+        if RF_type == 'electric_transverse':
+            set_name += 'ERF_' + str(RF_amplitude)
+        elif RF_type == 'magnetic_transverse':
+            set_name += 'BRF_' + str(RF_amplitude)
+        if induced_fields_factor < 1.0:
+            set_name += '_iff' + str(induced_fields_factor)
+        if with_kr_correction == True:
+            set_name += '_withkrcor'
+        set_name += '_tcycdivs' + str(time_step_tau_cyclotron_divisions)
+        if sigma_r0 > 0:
+            set_name += '_sigmar' + str(sigma_r0)
+            if radial_distribution == 'normal':
+                set_name += 'norm'
+            elif radial_distribution == 'uniform':
+                set_name += 'unif'
+        set_name += '_' + gas_name
+        print(set_name)
+        single_particle_file = single_particle_dir + '/' + set_name + '.mat'
 
-            for num_cells in num_cells_list:
-                run_name = plasma_mode
-                run_name += '_' + gas_name
-                # RF_label = 'RF_terms' \
-                #            + '_rc_' + str(RF_rc_curr) \
-                #            + '_lc_' + str(RF_lc_curr) \
-                #            + '_cr_' + str(RF_cr_curr) \
-                #            + '_cl_' + str(RF_cl_curr) \
-                #            + '_rl_' + str(RF_rl_curr) \
-                #            + '_lr_' + str(RF_lr_curr)
-                RF_label = 'alpha_' + str(alpha) + '_beta_' + str(beta)
-                run_name += '_' + RF_label
-                run_name += '_N_' + str(num_cells)
+        RF_rates_mat_dict = loadmat(single_particle_file)
+        alpha_loop_list = RF_rates_mat_dict['alpha_loop_list'][0]
+        beta_loop_list = RF_rates_mat_dict['beta_loop_list'][0]
+        N_rc_1 = RF_rates_mat_dict['N_rc']
+        N_lc_1 = RF_rates_mat_dict['N_lc']
+        N_cr_1 = RF_rates_mat_dict['N_cr']
+        N_cl_1 = RF_rates_mat_dict['N_cl']
+        N_rl_1 = RF_rates_mat_dict['N_rl']
+        N_lr_1 = RF_rates_mat_dict['N_lr']
 
-                print('run_name = ' + run_name)
+        total_number_of_combinations = len(num_cells_list) * len(alpha_loop_list) * len(beta_loop_list)
+        print('total_number_of_combinations = ' + str(total_number_of_combinations))
+        cnt = 0
 
-                settings = {}
-                settings['gas_name'] = gas_name
+        # create corresponding dir in the mm_rate_eqs
+        sub_folder = main_folder + '/' + set_name
+        print('creating sub_folder:', sub_folder)
+        os.makedirs(sub_folder, exist_ok=True)
 
-                settings = define_default_settings(settings)
-                settings['draw_plots'] = False  # plotting not possible on slurm computers without display
+        for ind_beta, beta in enumerate(beta_loop_list):
+            for ind_alpha, alpha in enumerate(alpha_loop_list):
 
-                if plasma_mode == 'isoTmfp':
-                    settings['assume_constant_density'] = True
-                    settings['assume_constant_temperature'] = True
-                elif plasma_mode == 'isoT':
-                    settings['assume_constant_density'] = False
-                    settings['assume_constant_temperature'] = True
-                elif 'cool' in plasma_mode:
-                    settings['assume_constant_density'] = False
-                    settings['assume_constant_temperature'] = False
-                    settings['plasma_dimension'] = int(plasma_mode.split('d')[-1])
+                RF_rc_curr = RF_rates_mat_dict['N_rc'][ind_beta, ind_alpha]
+                RF_lc_curr = RF_rates_mat_dict['N_lc'][ind_beta, ind_alpha]
+                RF_cr_curr = RF_rates_mat_dict['N_cr'][ind_beta, ind_alpha]
+                RF_cl_curr = RF_rates_mat_dict['N_cl'][ind_beta, ind_alpha]
+                RF_rl_curr = RF_rates_mat_dict['N_rl'][ind_beta, ind_alpha]
+                RF_lr_curr = RF_rates_mat_dict['N_lr'][ind_beta, ind_alpha]
 
-                settings['n0'] = n0
-                settings['Ti_0'] = Ti
-                settings['Te_0'] = Ti
+                for num_cells in num_cells_list:
+                    run_name = plasma_mode
+                    run_name += '_' + gas_name
+                    # RF_label = 'RF_terms' \
+                    #            + '_rc_' + str(RF_rc_curr) \
+                    #            + '_lc_' + str(RF_lc_curr) \
+                    #            + '_cr_' + str(RF_cr_curr) \
+                    #            + '_cl_' + str(RF_cl_curr) \
+                    #            + '_rl_' + str(RF_rl_curr) \
+                    #            + '_lr_' + str(RF_lr_curr)
+                    RF_label = 'alpha_' + str(alpha) + '_beta_' + str(beta)
+                    run_name += '_' + RF_label
+                    run_name += '_N_' + str(num_cells)
 
-                settings['cell_size'] = 1.0  # m
+                    print('run_name = ' + run_name)
 
-                # settings['flux_normalized_termination_cutoff'] = 0.05
-                # settings['flux_normalized_termination_cutoff'] = 0.01
-                settings['flux_normalized_termination_cutoff'] = 1e-3
-                # settings['flux_normalized_termination_cutoff'] = 1e-4
+                    settings = {}
+                    settings['gas_name'] = gas_name
 
-                # for const density right boundary condition
-                settings['right_boundary_condition'] = 'none'
-                # settings['right_boundary_condition'] = 'adjust_ntL_for_nend'
-                # settings['right_boundary_condition'] = 'adjust_ntR_for_nend'
-                # settings['right_boundary_condition'] = 'adjust_all_species_for_nend'
-                # settings['right_boundary_condition_density_type'] = 'n_expander'
-                # settings['n_expander_factor'] = 1e-2
-                # settings['n_min'] = n0 * 1e-3
-                # settings['time_step_definition_using_species'] = 'only_c_tR'
+                    settings = define_default_settings(settings)
+                    settings['draw_plots'] = False  # plotting not possible on slurm computers without display
 
-                settings['number_of_cells'] = num_cells
+                    if plasma_mode == 'isoTmfp':
+                        settings['assume_constant_density'] = True
+                        settings['assume_constant_temperature'] = True
+                    elif plasma_mode == 'isoT':
+                        settings['assume_constant_density'] = False
+                        settings['assume_constant_temperature'] = True
+                    elif 'cool' in plasma_mode:
+                        settings['assume_constant_density'] = False
+                        settings['assume_constant_temperature'] = False
+                        settings['plasma_dimension'] = int(plasma_mode.split('d')[-1])
 
-                # settings['transition_type'] = 'smooth_transition_to_free_flow'
-                settings['transition_type'] = 'none'
+                    settings['n0'] = n0
+                    settings['Ti_0'] = Ti
+                    settings['Te_0'] = Ti
 
-                # settings['Rm'] = 3.0
-                # settings['Rm'] = 6.0
-                settings['Rm'] = 10.0
+                    settings['cell_size'] = 1.0  # m
 
-                settings['use_RF_terms'] = True
-                settings['RF_rc'] = RF_rc_curr
-                settings['RF_lc'] = RF_lc_curr
-                settings['RF_cr'] = RF_cr_curr
-                settings['RF_cl'] = RF_cl_curr
-                settings['RF_rl'] = RF_rl_curr
-                settings['RF_lr'] = RF_lr_curr
+                    # settings['flux_normalized_termination_cutoff'] = 0.05
+                    # settings['flux_normalized_termination_cutoff'] = 0.01
+                    settings['flux_normalized_termination_cutoff'] = 1e-3
+                    # settings['flux_normalized_termination_cutoff'] = 1e-4
 
-                settings['save_dir'] = sub_folder + '/' + run_name
-                print('save dir: ' + str(settings['save_dir']))
-                os.makedirs(settings['save_dir'], exist_ok=True)
-                os.chdir(settings['save_dir'])
+                    # for const density right boundary condition
+                    settings['right_boundary_condition'] = 'none'
+                    # settings['right_boundary_condition'] = 'adjust_ntL_for_nend'
+                    # settings['right_boundary_condition'] = 'adjust_ntR_for_nend'
+                    # settings['right_boundary_condition'] = 'adjust_all_species_for_nend'
+                    # settings['right_boundary_condition_density_type'] = 'n_expander'
+                    # settings['n_expander_factor'] = 1e-2
+                    # settings['n_min'] = n0 * 1e-3
+                    # settings['time_step_definition_using_species'] = 'only_c_tR'
 
-                command = rate_eqs_script + ' --settings "' + str(settings) + '"'
-                s = Slurm(run_name, slurm_kwargs=slurm_kwargs)
-                s.run(command)
-                cnt += 1
-                print('run # ' + str(cnt) + ' / ' + str(total_number_of_combinations))
+                    settings['number_of_cells'] = num_cells
 
-                os.chdir(pwd)
+                    # settings['transition_type'] = 'smooth_transition_to_free_flow'
+                    settings['transition_type'] = 'none'
+
+                    # settings['Rm'] = 3.0
+                    # settings['Rm'] = 6.0
+                    settings['Rm'] = 10.0
+
+                    settings['use_RF_terms'] = True
+                    settings['RF_rc'] = RF_rc_curr
+                    settings['RF_lc'] = RF_lc_curr
+                    settings['RF_cr'] = RF_cr_curr
+                    settings['RF_cl'] = RF_cl_curr
+                    settings['RF_rl'] = RF_rl_curr
+                    settings['RF_lr'] = RF_lr_curr
+
+                    settings['save_dir'] = sub_folder + '/' + run_name
+                    print('save dir: ' + str(settings['save_dir']))
+
+                    state_file = settings['save_dir'] + '/state.pickle'
+                    if os.path.exists(state_file):
+                        print('exists, skipping.')
+                    else:
+                        os.makedirs(settings['save_dir'], exist_ok=True)
+                        os.chdir(settings['save_dir'])
+
+                        command = rate_eqs_script + ' --settings "' + str(settings) + '"'
+                        s = Slurm(run_name, slurm_kwargs=slurm_kwargs)
+                        s.run(command)
+                        cnt += 1
+                        print('run # ' + str(cnt) + ' / ' + str(total_number_of_combinations))
+
+                        os.chdir(pwd)
