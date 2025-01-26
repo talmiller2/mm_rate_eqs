11b0f81135586ff834d6b34b75dc8029b37a5f2e
talmiller2 <talmiller@gmail.com>
2025-01-26 07:34:45 +0200

    update set names in compile_parametric_N_RMF_from_mat

--- run_scripts/compile_parametric_N_RMF_from_mat.py
+++ run_scripts/compile_parametric_N_RMF_from_mat.py
@@ -11,18 +11,24 @@ n0 = 1e21  # m^-3
 Ti = 10 * 1e3  # eV
 main_folder = '/home/talm/code/mm_rate_eqs/runs/slurm_runs/'
 # main_folder += 'set47_MM_Rm_10_ni_1e21_Ti_10keV_withRMF'
-main_folder += 'set48_MM_Rm_10_ni_1e21_Ti_10keV_withRMF_zeroRL_fluxeps1e-2'
+# main_folder += 'set48_MM_Rm_10_ni_1e21_Ti_10keV_withRMF_zeroRL_fluxeps1e-2'
+main_folder += 'set54_MM_Rm_10_ni_1e20_Ti_10keV_smooth_fluxeps1e-3'
+# main_folder += 'set55_MM_Rm_10_ni_1e20_Ti_10keV_smooth_fluxeps1e-3'
+# main_folder += 'set56_MM_Rm_10_ni_1e20_Ti_10keV_smooth_fluxeps1e-3'
 
 plasma_mode = 'isoT'
 
-# num_cells_list = [10, 30, 50]
+num_cells_list = [10, 30, 50]
 # num_cells_list = [30]
-num_cells_list = [50]
+# num_cells_list = [50]
 
 # load single_particle compiled mat
 # single_particle_dir = '/Users/talmiller/Downloads/single_particle/'
 single_particle_dir = '/home/talm/code/single_particle/slurm_runs/'
-single_particle_dir += '/set53_B0_1T_l_1m_Post_Rm_10_intervals_D_T/'
+# single_particle_dir += '/set53_B0_1T_l_1m_Post_Rm_10_intervals_D_T/'
+single_particle_dir += '/set54_B0_1T_l_1m_Post_Rm_10_intervals_D_T/'
+# single_particle_dir += '/set55_B0_1T_l_1m_Post_Rm_10_intervals_D_T/'
+# single_particle_dir += '/set56_B0_1T_l_1m_Post_Rm_10_intervals_D_T/'
 
 gas_name_list = ['deuterium', 'tritium']
 
