67651e81b885feaa163b3b4c24615dcdc9a04ca7
talmiller2 <talmiller@gmail.com>
2025-12-04 16:33:44 +0200

    add sigma_r0_list the loop as well

--- run_scripts/compile_parametric_RF_mat.py
+++ run_scripts/compile_parametric_RF_mat.py
@@ -14,7 +14,8 @@ main_folder = '/home/talm/code/mm_rate_eqs/runs/slurm_runs/'
 # main_folder += 'set55_MM_Rm_10_ni_1e21_Ti_10keV_smooth_fluxeps1e-3'
 # main_folder += 'set56_MM_Rm_10_ni_1e21_Ti_10keV_smooth_fluxeps1e-3'
 # main_folder += 'set56_MM_Rm_10_ni_1e21_Ti_10keV_smooth_zeroRL_fluxeps1e-3'
-main_folder += '/set59_MMwithRF_Rm_5_ni_1e21_Ti_10keV_smooth/'
+# main_folder += '/set59_MMwithRF_Rm_5_ni_1e21_Ti_10keV_smooth/'
+main_folder += '/set61_MMwithRF_Rm_5_ni_1e21_Ti_10keV_smooth/'
 
 plasma_mode = 'isoT'
 
@@ -22,8 +23,9 @@ plasma_mode = 'isoT'
 # num_cells_list = [10, 30, 50, 80]
 # num_cells_list = [30]
 # num_cells_list = [50]
-num_cells_list = [10, 20, 30, 40, 50]
+# num_cells_list = [10, 20, 30, 40, 50]
 # num_cells_list = [60, 70, 80]
+num_cells_list = [10, 20, 30, 40, 50, 60, 70, 80]
 
 # load single_particle compiled mat
 # single_particle_dir = '/Users/talmiller/Downloads/single_particle/'
@@ -32,7 +34,8 @@ single_particle_dir = '/home/talm/code/single_particle/slurm_runs/'
 # single_particle_dir += '/set54_B0_1T_l_1m_Post_Rm_10_intervals_D_T/'
 # single_particle_dir += '/set55_B0_1T_l_1m_Post_Rm_10_intervals_D_T/'
 # single_particle_dir += '/set56_B0_1T_l_1m_Post_Rm_10_intervals_D_T/'
-single_particle_dir += '/set59_B0_1T_l_1m_Post_Rm_5_r0max_30cm/'
+# single_particle_dir += '/set59_B0_1T_l_1m_Post_Rm_5_r0max_30cm/'
+single_particle_dir += '/set61_B0_1T_l_1m_Post_Rm_5_r0max_10cm/'
 
 gas_name_list = ['deuterium', 'tritium']
 
@@ -41,10 +44,10 @@ RF_amplitude_list = []
 induced_fields_factor_list = []
 with_kr_correction_list = []
 
-# RF_type_list += ['electric_transverse']
-# RF_amplitude_list += [25]  # kV/m
-# induced_fields_factor_list += [1]
-# with_kr_correction_list += [True]
+RF_type_list += ['electric_transverse']
+RF_amplitude_list += [25]  # kV/m
+induced_fields_factor_list += [1]
+with_kr_correction_list += [True]
 
 RF_type_list += ['electric_transverse']
 RF_amplitude_list += [50]  # kV/m
@@ -61,127 +64,152 @@ with_kr_correction_list += [True]
 # induced_fields_factor_list += [0]
 # with_kr_correction_list += [True]
 
+# RF_type_list += ['magnetic_transverse']
+# RF_amplitude_list += [0.04]  # T
+# induced_fields_factor_list += [1]
+# with_kr_correction_list += [True]
+#
+# RF_type_list += ['magnetic_transverse']
+# RF_amplitude_list += [0.04]  # T
+# induced_fields_factor_list += [0]
+# with_kr_correction_list += [True]
+
+
 RF_type_list += ['magnetic_transverse']
-RF_amplitude_list += [0.04]  # T
+RF_amplitude_list += [0.025]  # T
 induced_fields_factor_list += [1]
 with_kr_correction_list += [True]
 
 RF_type_list += ['magnetic_transverse']
-RF_amplitude_list += [0.04]  # T
+RF_amplitude_list += [0.025]  # T
 induced_fields_factor_list += [0]
 with_kr_correction_list += [True]
 
-total_number_of_sets = len(RF_type_list) * len(gas_name_list)
+RF_type_list += ['magnetic_transverse']
+RF_amplitude_list += [0.05]  # T
+induced_fields_factor_list += [1]
+with_kr_correction_list += [True]
+
+RF_type_list += ['magnetic_transverse']
+RF_amplitude_list += [0.05]  # T
+induced_fields_factor_list += [0]
+with_kr_correction_list += [True]
+
+# sigma_r0 = 0
+# sigma_r0 = 0.05
+# sigma_r0 = 0.1
+# sigma_r0 = 0.3
+sigma_r0_list = [0, 0.1]
+
+total_number_of_sets = len(RF_type_list) * len(gas_name_list) * len(sigma_r0_list)
 print('total_number_of_sets = ' + str(total_number_of_sets))
 cnt_sets = 0
 
 for RF_type, RF_amplitude, induced_fields_factor, with_kr_correction \
         in zip(RF_type_list, RF_amplitude_list, induced_fields_factor_list, with_kr_correction_list):
     for gas_name in gas_name_list:
+        for sigma_r0 in sigma_r0_list:
 
-        cnt_sets += 1
-        print('@@@@@@@ set num', cnt_sets, '/', total_number_of_sets)
+            cnt_sets += 1
+            print('@@@@@@@ set num', cnt_sets, '/', total_number_of_sets)
 
-        time_step_tau_cyclotron_divisions = 50
-        # time_step_tau_cyclotron_divisions = 100
-        # sigma_r0 = 0
-        # sigma_r0 = 0.05
-        # sigma_r0 = 0.1
-        sigma_r0 = 0.3
-        radial_distribution = 'uniform'
+            time_step_tau_cyclotron_divisions = 50
+            # time_step_tau_cyclotron_divisions = 100
 
-        # theta_type = 'sign_vz0'
-        theta_type = 'sign_vz'
+            radial_distribution = 'uniform'
 
-        if gas_name == 'deuterium':
-            gas_name_short = 'D'
-        elif gas_name == 'tritium':
-            gas_name_short = 'T'
-        else:
-            gas_name_short = 'DTmix'
+            # theta_type = 'sign_vz0'
+            theta_type = 'sign_vz'
 
-        # set_name = 'compiled_'
-        set_name = 'smooth_compiled_'
-        set_name += theta_type + '_'
-        if RF_type == 'electric_transverse':
-            set_name += 'ERF_' + str(RF_amplitude)
-        elif RF_type == 'magnetic_transverse':
-            set_name += 'BRF_' + str(RF_amplitude)
-        if induced_fields_factor < 1.0:
-            set_name += '_iff' + str(induced_fields_factor)
-        if with_kr_correction == True:
-            set_name += '_withkrcor'
-        set_name += '_tcycdivs' + str(time_step_tau_cyclotron_divisions)
-        if sigma_r0 > 0:
-            set_name += '_sigmar' + str(sigma_r0)
-            if radial_distribution == 'normal':
-                set_name += 'norm'
-            elif radial_distribution == 'uniform':
-                set_name += 'unif'
-        set_name += '_' + gas_name
-        print(set_name)
-        single_particle_file = single_particle_dir + '/' + set_name + '.mat'
+            if gas_name == 'deuterium':
+                gas_name_short = 'D'
+            elif gas_name == 'tritium':
+                gas_name_short = 'T'
+            else:
+                gas_name_short = 'DTmix'
 
-        try:
+            # set_name = 'compiled_'
+            set_name = 'smooth_compiled_'
+            set_name += theta_type + '_'
+            if RF_type == 'electric_transverse':
+                set_name += 'ERF_' + str(RF_amplitude)
+            elif RF_type == 'magnetic_transverse':
+                set_name += 'BRF_' + str(RF_amplitude)
+            if induced_fields_factor < 1.0:
+                set_name += '_iff' + str(induced_fields_factor)
+            if with_kr_correction == True:
+                set_name += '_withkrcor'
+            set_name += '_tcycdivs' + str(time_step_tau_cyclotron_divisions)
+            if sigma_r0 > 0:
+                set_name += '_sigmar' + str(sigma_r0)
+                if radial_distribution == 'normal':
+                    set_name += 'norm'
+                elif radial_distribution == 'uniform':
+                    set_name += 'unif'
+            set_name += '_' + gas_name
+            print(set_name)
+            single_particle_file = single_particle_dir + '/' + set_name + '.mat'
 
-            RF_rates_mat_dict = loadmat(single_particle_file)
-            alpha_loop_list = RF_rates_mat_dict['alpha_loop_list'][0]
-            beta_loop_list = RF_rates_mat_dict['beta_loop_list'][0]
+            try:
 
-            total_number_of_combinations = len(num_cells_list) * len(alpha_loop_list) * len(beta_loop_list)
-            print('total_number_of_combinations = ' + str(total_number_of_combinations))
-            cnt = 0
+                RF_rates_mat_dict = loadmat(single_particle_file)
+                alpha_loop_list = RF_rates_mat_dict['alpha_loop_list'][0]
+                beta_loop_list = RF_rates_mat_dict['beta_loop_list'][0]
 
-            sub_folder = main_folder + '/' + set_name
+                total_number_of_combinations = len(num_cells_list) * len(alpha_loop_list) * len(beta_loop_list)
+                print('total_number_of_combinations = ' + str(total_number_of_combinations))
+                cnt = 0
 
-            for num_cells in num_cells_list:
+                sub_folder = main_folder + '/' + set_name
 
-                # initialize mats
-                flux_mat = np.nan * RF_rates_mat_dict['N_rc_end']
+                for num_cells in num_cells_list:
 
-                density_profiles = {}
-                population_keys = ['n', 'n_c', 'n_tL', 'n_tR']
-                for pop in population_keys:
-                    density_profiles[pop] = np.zeros([len(beta_loop_list), len(alpha_loop_list), num_cells])
+                    # initialize mats
+                    flux_mat = np.nan * RF_rates_mat_dict['N_rc_end']
 
-                for ind_beta, beta in enumerate(beta_loop_list):
-                    for ind_alpha, alpha in enumerate(alpha_loop_list):
-                        cnt += 1
+                    density_profiles = {}
+                    population_keys = ['n', 'n_c', 'n_tL', 'n_tR']
+                    for pop in population_keys:
+                        density_profiles[pop] = np.zeros([len(beta_loop_list), len(alpha_loop_list), num_cells])
 
-                        run_name = plasma_mode
-                        run_name += '_' + gas_name
-                        RF_label = 'alpha_' + str(alpha) + '_beta_' + str(beta)
-                        run_name += '_' + RF_label
-                        run_name += '_N_' + str(num_cells)
+                    for ind_beta, beta in enumerate(beta_loop_list):
+                        for ind_alpha, alpha in enumerate(alpha_loop_list):
+                            cnt += 1
 
-                        state_file = sub_folder + '/' + run_name + '/state.pickle'
+                            run_name = plasma_mode
+                            run_name += '_' + gas_name
+                            RF_label = 'alpha_' + str(alpha) + '_beta_' + str(beta)
+                            run_name += '_' + RF_label
+                            run_name += '_N_' + str(num_cells)
 
-                        if os.path.exists(state_file):
-                            print('loading state of run # ' + str(cnt) + ' / ' + str(total_number_of_combinations))
-                            try:
-                                with open(state_file, 'rb') as fid:
-                                    state = pickle.load(fid)
-                                flux_mat[ind_beta, ind_alpha] = state['flux_mean']
-                                for pop in population_keys:
-                                    density_profiles[pop][ind_beta, ind_alpha, :] = state[pop]
+                            state_file = sub_folder + '/' + run_name + '/state.pickle'
 
-                            except:
-                                print('FAILED TO LOAD.')
-                        else:
-                            print(state_file, 'doesnt exist.')
+                            if os.path.exists(state_file):
+                                print('loading state of run # ' + str(cnt) + ' / ' + str(total_number_of_combinations))
+                                try:
+                                    with open(state_file, 'rb') as fid:
+                                        state = pickle.load(fid)
+                                    flux_mat[ind_beta, ind_alpha] = state['flux_mean']
+                                    for pop in population_keys:
+                                        density_profiles[pop][ind_beta, ind_alpha, :] = state[pop]
 
-                # save data
-                save_mat_dict = {}
-                save_mat_dict['alpha_loop_list'] = alpha_loop_list
-                save_mat_dict['beta_loop_list'] = beta_loop_list
-                save_mat_dict['flux_mat'] = flux_mat
-                for pop in population_keys:
-                    save_mat_dict[pop] = density_profiles[pop]
+                                except:
+                                    print('FAILED TO LOAD.')
+                            else:
+                                print(state_file, 'doesnt exist.')
 
-                compiled_save_file = main_folder + '/' + set_name + '_N_' + str(num_cells) + '.mat'
-                print('saving', compiled_save_file)
-                savemat(compiled_save_file, save_mat_dict)
+                    # save data
+                    save_mat_dict = {}
+                    save_mat_dict['alpha_loop_list'] = alpha_loop_list
+                    save_mat_dict['beta_loop_list'] = beta_loop_list
+                    save_mat_dict['flux_mat'] = flux_mat
+                    for pop in population_keys:
+                        save_mat_dict[pop] = density_profiles[pop]
 
-        except:
-            print('failed to compile', set_name, '- skipping.')
-            pass
+                    compiled_save_file = main_folder + '/' + set_name + '_N_' + str(num_cells) + '.mat'
+                    print('saving', compiled_save_file)
+                    savemat(compiled_save_file, save_mat_dict)
+
+            except:
+                print('failed to compile', set_name, '- skipping.')
+                pass
--- run_scripts/run_slurm_parametric_RF_mat.py
+++ run_scripts/run_slurm_parametric_RF_mat.py
@@ -124,162 +124,165 @@ RF_amplitude_list += [0.05]  # T
 induced_fields_factor_list += [0]
 with_kr_correction_list += [True]
 
-total_number_of_sets = len(RF_type_list) * len(gas_name_list)
+# sigma_r0 = 0
+# sigma_r0 = 0.05
+# sigma_r0 = 0.1
+# sigma_r0 = 0.3
+sigma_r0_list = [0, 0.1]
+
+total_number_of_sets = len(RF_type_list) * len(gas_name_list) * len(sigma_r0_list)
 print('total_number_of_sets = ' + str(total_number_of_sets))
 cnt_sets = 0
 
 for RF_type, RF_amplitude, induced_fields_factor, with_kr_correction \
         in zip(RF_type_list, RF_amplitude_list, induced_fields_factor_list, with_kr_correction_list):
     for gas_name in gas_name_list:
+        for sigma_r0 in sigma_r0_list:
+            cnt_sets += 1
+            print('@@@@@@@ set num', cnt_sets, '/', total_number_of_sets)
 
-        cnt_sets += 1
-        print('@@@@@@@ set num', cnt_sets, '/', total_number_of_sets)
+            time_step_tau_cyclotron_divisions = 50
+            # time_step_tau_cyclotron_divisions = 100
 
-        time_step_tau_cyclotron_divisions = 50
-        # time_step_tau_cyclotron_divisions = 100
-        # sigma_r0 = 0
-        # sigma_r0 = 0.05
-        sigma_r0 = 0.1
-        # sigma_r0 = 0.3
-        radial_distribution = 'uniform'
+            radial_distribution = 'uniform'
 
-        # theta_type = 'sign_vz0'
-        theta_type = 'sign_vz'
+            # theta_type = 'sign_vz0'
+            theta_type = 'sign_vz'
 
-        if gas_name == 'deuterium':
-            gas_name_short = 'D'
-        elif gas_name == 'tritium':
-            gas_name_short = 'T'
-        else:
-            gas_name_short = 'DTmix'
+            if gas_name == 'deuterium':
+                gas_name_short = 'D'
+            elif gas_name == 'tritium':
+                gas_name_short = 'T'
+            else:
+                gas_name_short = 'DTmix'
 
-        # set_name = 'compiled_'
-        set_name = 'smooth_compiled_'
-        set_name += theta_type + '_'
-        if RF_type == 'electric_transverse':
-            set_name += 'ERF_' + str(RF_amplitude)
-        elif RF_type == 'magnetic_transverse':
-            set_name += 'BRF_' + str(RF_amplitude)
-        if induced_fields_factor < 1.0:
-            set_name += '_iff' + str(induced_fields_factor)
-        if with_kr_correction == True:
-            set_name += '_withkrcor'
-        set_name += '_tcycdivs' + str(time_step_tau_cyclotron_divisions)
-        if sigma_r0 > 0:
-            set_name += '_sigmar' + str(sigma_r0)
-            if radial_distribution == 'normal':
-                set_name += 'norm'
-            elif radial_distribution == 'uniform':
-                set_name += 'unif'
-        set_name += '_' + gas_name
-        print(set_name)
-        single_particle_file = single_particle_dir + '/' + set_name + '.mat'
+            # set_name = 'compiled_'
+            set_name = 'smooth_compiled_'
+            set_name += theta_type + '_'
+            if RF_type == 'electric_transverse':
+                set_name += 'ERF_' + str(RF_amplitude)
+            elif RF_type == 'magnetic_transverse':
+                set_name += 'BRF_' + str(RF_amplitude)
+            if induced_fields_factor < 1.0:
+                set_name += '_iff' + str(induced_fields_factor)
+            if with_kr_correction == True:
+                set_name += '_withkrcor'
+            set_name += '_tcycdivs' + str(time_step_tau_cyclotron_divisions)
+            if sigma_r0 > 0:
+                set_name += '_sigmar' + str(sigma_r0)
+                if radial_distribution == 'normal':
+                    set_name += 'norm'
+                elif radial_distribution == 'uniform':
+                    set_name += 'unif'
+            set_name += '_' + gas_name
+            print(set_name)
+            single_particle_file = single_particle_dir + '/' + set_name + '.mat'
 
-        RF_rates_mat_dict = loadmat(single_particle_file)
-        alpha_loop_list = RF_rates_mat_dict['alpha_loop_list'][0]
-        beta_loop_list = RF_rates_mat_dict['beta_loop_list'][0]
+            RF_rates_mat_dict = loadmat(single_particle_file)
+            alpha_loop_list = RF_rates_mat_dict['alpha_loop_list'][0]
+            beta_loop_list = RF_rates_mat_dict['beta_loop_list'][0]
 
-        total_number_of_combinations = len(num_cells_list) * len(alpha_loop_list) * len(beta_loop_list)
-        print('total_number_of_combinations = ' + str(total_number_of_combinations))
-        cnt = 0
+            total_number_of_combinations = len(num_cells_list) * len(alpha_loop_list) * len(beta_loop_list)
+            print('total_number_of_combinations = ' + str(total_number_of_combinations))
+            cnt = 0
 
-        # create corresponding dir in the mm_rate_eqs
-        sub_folder = main_folder + '/' + set_name
-        print('creating sub_folder:', sub_folder)
-        os.makedirs(sub_folder, exist_ok=True)
+            # create corresponding dir in the mm_rate_eqs
+            sub_folder = main_folder + '/' + set_name
+            print('creating sub_folder:', sub_folder)
+            os.makedirs(sub_folder, exist_ok=True)
 
-        for ind_beta, beta in enumerate(beta_loop_list):
-            for ind_alpha, alpha in enumerate(alpha_loop_list):
+            for ind_beta, beta in enumerate(beta_loop_list):
+                for ind_alpha, alpha in enumerate(alpha_loop_list):
 
-                RF_rc_curr = RF_rates_mat_dict['N_rc_end'][ind_beta, ind_alpha]
-                RF_lc_curr = RF_rates_mat_dict['N_lc_end'][ind_beta, ind_alpha]
-                RF_cr_curr = RF_rates_mat_dict['N_cr_end'][ind_beta, ind_alpha]
-                RF_cl_curr = RF_rates_mat_dict['N_cl_end'][ind_beta, ind_alpha]
-                RF_rl_curr = RF_rates_mat_dict['N_rl_end'][ind_beta, ind_alpha]
-                RF_lr_curr = RF_rates_mat_dict['N_lr_end'][ind_beta, ind_alpha]
-                # RF_rl_curr = 0
-                # RF_lr_curr = 0
+                    RF_rc_curr = RF_rates_mat_dict['N_rc_end'][ind_beta, ind_alpha]
+                    RF_lc_curr = RF_rates_mat_dict['N_lc_end'][ind_beta, ind_alpha]
+                    RF_cr_curr = RF_rates_mat_dict['N_cr_end'][ind_beta, ind_alpha]
+                    RF_cl_curr = RF_rates_mat_dict['N_cl_end'][ind_beta, ind_alpha]
+                    RF_rl_curr = RF_rates_mat_dict['N_rl_end'][ind_beta, ind_alpha]
+                    RF_lr_curr = RF_rates_mat_dict['N_lr_end'][ind_beta, ind_alpha]
+                    # RF_rl_curr = 0
+                    # RF_lr_curr = 0
 
-                for num_cells in num_cells_list:
-                    run_name = plasma_mode
-                    run_name += '_' + gas_name
-                    RF_label = 'alpha_' + str(alpha) + '_beta_' + str(beta)
-                    run_name += '_' + RF_label
-                    run_name += '_N_' + str(num_cells)
+                    for num_cells in num_cells_list:
+                        run_name = plasma_mode
+                        run_name += '_' + gas_name
+                        RF_label = 'alpha_' + str(alpha) + '_beta_' + str(beta)
+                        run_name += '_' + RF_label
+                        run_name += '_N_' + str(num_cells)
 
-                    print('run_name = ' + run_name)
+                        print('run_name = ' + run_name)
 
-                    settings = {}
-                    settings['gas_name'] = gas_name
+                        settings = {}
+                        settings['gas_name'] = gas_name
 
-                    settings = define_default_settings(settings)
-                    settings['draw_plots'] = False  # plotting not possible on slurm computers without display
+                        settings = define_default_settings(settings)
+                        settings['draw_plots'] = False  # plotting not possible on slurm computers without display
 
-                    if plasma_mode == 'isoTmfp':
-                        settings['assume_constant_density'] = True
-                        settings['assume_constant_temperature'] = True
-                    elif plasma_mode == 'isoT':
-                        settings['assume_constant_density'] = False
-                        settings['assume_constant_temperature'] = True
-                    elif 'cool' in plasma_mode:
-                        settings['assume_constant_density'] = False
-                        settings['assume_constant_temperature'] = False
-                        settings['plasma_dimension'] = int(plasma_mode.split('d')[-1])
+                        if plasma_mode == 'isoTmfp':
+                            settings['assume_constant_density'] = True
+                            settings['assume_constant_temperature'] = True
+                        elif plasma_mode == 'isoT':
+                            settings['assume_constant_density'] = False
+                            settings['assume_constant_temperature'] = True
+                        elif 'cool' in plasma_mode:
+                            settings['assume_constant_density'] = False
+                            settings['assume_constant_temperature'] = False
+                            settings['plasma_dimension'] = int(plasma_mode.split('d')[-1])
 
-                    settings['n0'] = n0
-                    settings['Ti_0'] = Ti
-                    settings['Te_0'] = Ti
+                        settings['n0'] = n0
+                        settings['Ti_0'] = Ti
+                        settings['Te_0'] = Ti
 
-                    # settings['cell_size'] = 1.0  # m
-                    settings['cell_size'] = l
+                        # settings['cell_size'] = 1.0  # m
+                        settings['cell_size'] = l
 
-                    # settings['flux_normalized_termination_cutoff'] = 0.05
-                    # settings['flux_normalized_termination_cutoff'] = 1e-2
-                    settings['flux_normalized_termination_cutoff'] = 1e-3
-                    # settings['flux_normalized_termination_cutoff'] = 1e-4
+                        # settings['flux_normalized_termination_cutoff'] = 0.05
+                        # settings['flux_normalized_termination_cutoff'] = 1e-2
+                        settings['flux_normalized_termination_cutoff'] = 1e-3
+                        # settings['flux_normalized_termination_cutoff'] = 1e-4
 
-                    # for const density right boundary condition
-                    settings['right_boundary_condition'] = 'none'
-                    # settings['right_boundary_condition'] = 'adjust_ntL_for_nend'
-                    # settings['right_boundary_condition'] = 'adjust_ntR_for_nend'
-                    # settings['right_boundary_condition'] = 'adjust_all_species_for_nend'
-                    # settings['right_boundary_condition_density_type'] = 'n_expander'
-                    # settings['n_expander_factor'] = 1e-2
-                    # settings['n_min'] = n0 * 1e-3
-                    # settings['time_step_definition_using_species'] = 'only_c_tR'
+                        # for const density right boundary condition
+                        settings['right_boundary_condition'] = 'none'
+                        # settings['right_boundary_condition'] = 'adjust_ntL_for_nend'
+                        # settings['right_boundary_condition'] = 'adjust_ntR_for_nend'
+                        # settings['right_boundary_condition'] = 'adjust_all_species_for_nend'
+                        # settings['right_boundary_condition_density_type'] = 'n_expander'
+                        # settings['n_expander_factor'] = 1e-2
+                        # settings['n_min'] = n0 * 1e-3
+                        # settings['time_step_definition_using_species'] = 'only_c_tR'
 
-                    settings['number_of_cells'] = num_cells
+                        settings['number_of_cells'] = num_cells
 
-                    # settings['transition_type'] = 'smooth_transition_to_free_flow'
-                    settings['transition_type'] = 'none'
+                        # settings['transition_type'] = 'smooth_transition_to_free_flow'
+                        settings['transition_type'] = 'none'
 
-                    # settings['Rm'] = 3.0
-                    # settings['Rm'] = 6.0
-                    # settings['Rm'] = 10.0
-                    settings['Rm'] = Rm
+                        # settings['Rm'] = 3.0
+                        # settings['Rm'] = 6.0
+                        # settings['Rm'] = 10.0
+                        settings['Rm'] = Rm
 
-                    settings['use_RF_terms'] = True
-                    settings['RF_rc'] = RF_rc_curr
-                    settings['RF_lc'] = RF_lc_curr
-                    settings['RF_cr'] = RF_cr_curr
-                    settings['RF_cl'] = RF_cl_curr
-                    settings['RF_rl'] = RF_rl_curr
-                    settings['RF_lr'] = RF_lr_curr
+                        settings['use_RF_terms'] = True
+                        settings['RF_rc'] = RF_rc_curr
+                        settings['RF_lc'] = RF_lc_curr
+                        settings['RF_cr'] = RF_cr_curr
+                        settings['RF_cl'] = RF_cl_curr
+                        settings['RF_rl'] = RF_rl_curr
+                        settings['RF_lr'] = RF_lr_curr
 
-                    settings['save_dir'] = sub_folder + '/' + run_name
-                    print('save dir: ' + str(settings['save_dir']))
+                        settings['save_dir'] = sub_folder + '/' + run_name
+                        print('save dir: ' + str(settings['save_dir']))
 
-                    state_file = settings['save_dir'] + '/state.pickle'
-                    if os.path.exists(state_file):
-                        print('exists, skipping.')
-                    else:
-                        os.makedirs(settings['save_dir'], exist_ok=True)
-                        os.chdir(settings['save_dir'])
+                        state_file = settings['save_dir'] + '/state.pickle'
+                        if os.path.exists(state_file):
+                            print('exists, skipping.')
+                        else:
+                            os.makedirs(settings['save_dir'], exist_ok=True)
+                            os.chdir(settings['save_dir'])
 
-                        command = rate_eqs_script + ' --settings "' + str(settings) + '"'
-                        s = Slurm(run_name, slurm_kwargs=slurm_kwargs)
-                        s.run(command)
-                        cnt += 1
-                        print('run # ' + str(cnt) + ' / ' + str(total_number_of_combinations))
+                            command = rate_eqs_script + ' --settings "' + str(settings) + '"'
+                            s = Slurm(run_name, slurm_kwargs=slurm_kwargs)
+                            s.run(command)
+                            cnt += 1
+                            print('run # ' + str(cnt) + ' / ' + str(total_number_of_combinations))
 
-                        os.chdir(pwd)
+                            os.chdir(pwd)
