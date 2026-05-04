5539d1c7fe2ed563916caf3dd4931c7141531b49
talmiller2 <talmiller@gmail.com>
2026-05-04 18:07:42 +0300

    -changed the U_list range to be below 1 to avoid outliers -add ft_list=1,2 to loop -changes gas_name='DT-mix' and removed from file names

--- run_scripts/compile_parametric_MMM_v2.py
+++ run_scripts/compile_parametric_MMM_v2.py
@@ -10,18 +10,19 @@ rate_eqs_script = get_script_rate_eqs_slave()
 main_folder = '/home/talm/code/mm_rate_eqs/runs/slurm_runs/'
 main_folder += 'set62_MMM_ni_1e21_Ti_10keV_constmfp'
 
-# gas_name = 'deuterium'
-gas_name = 'tritium'
-
 num_cells_list = [30]
 # num_cells_list = [10, 20, 30, 40, 50, 60, 70, 80]
 
 Rm_list = np.arange(2, 10.25, 0.25)
-U_list = np.arange(0, 1.05, 0.05)
+U_list = np.arange(0, 0.95, 0.05)
 
 modes = []
 modes += ['mahmir']
 modes += ['mekel']
+
+ft_list = []
+ft_list += [1]
+ft_list += [2]
 
 total_number_of_sets = len(num_cells_list) * len(modes)
 print('total_number_of_sets = ' + str(total_number_of_sets))
@@ -29,53 +30,55 @@ print('total_number_of_sets = ' + str(total_number_of_sets))
 cnt_sets = 0
 for num_cells in num_cells_list:
     for mode in modes:
-        cnt_sets += 1
-        print('@@@@@@@ set num', cnt_sets, '/', total_number_of_sets)
+        for ft in ft_list:
 
-        # initialize mats
-        flux_mat = np.zeros([len(Rm_list), len(U_list)])
+            cnt_sets += 1
+            print('@@@@@@@ set num', cnt_sets, '/', total_number_of_sets)
 
-        density_profiles = {}
-        population_keys = ['n', 'n_c', 'n_tL', 'n_tR']
-        for pop in population_keys:
-            density_profiles[pop] = np.zeros([len(Rm_list), len(U_list), num_cells])
+            # initialize mats
+            flux_mat = np.zeros([len(Rm_list), len(U_list)])
 
-        # compile (Rm, U) into matrix
-        for ind_Rm, Rm in enumerate(Rm_list):
-            for ind_U, U in enumerate(U_list):
+            density_profiles = {}
+            population_keys = ['n', 'n_c', 'n_tL', 'n_tR']
+            for pop in population_keys:
+                density_profiles[pop] = np.zeros([len(Rm_list), len(U_list), num_cells])
 
-                set_name = 'N_' + str(num_cells)
-                set_name += '_mode_' + str(mode)
-                set_name += f'_Rm_{Rm:g}'
-                set_name += f'_U_{U:g}'
-                set_name += '_' + gas_name
+            # compile (Rm, U) into matrix
+            for ind_Rm, Rm in enumerate(Rm_list):
+                for ind_U, U in enumerate(U_list):
 
-                state_file = main_folder + '/' + set_name + '/state.pickle'
-                if os.path.exists(state_file):
-                    try:
-                        with open(state_file, 'rb') as fid:
-                            state = pickle.load(fid)
-                        flux_mat[ind_Rm, ind_U] = state['flux_mean']
-                        for pop in population_keys:
-                            density_profiles[pop][ind_Rm, ind_U, :] = state[pop]
+                    set_name = 'N_' + str(num_cells)
+                    set_name += '_mode_' + str(mode)
+                    set_name += f'_ft_{ft:g}'
+                    set_name += f'_Rm_{Rm:g}'
+                    set_name += f'_U_{U:g}'
 
-                    except:
-                        print('failed to load', state_file)
-                else:
-                    print(state_file, 'doesnt exist.')
+                    state_file = main_folder + '/' + set_name + '/state.pickle'
+                    if os.path.exists(state_file):
+                        try:
+                            with open(state_file, 'rb') as fid:
+                                state = pickle.load(fid)
+                            flux_mat[ind_Rm, ind_U] = state['flux_mean']
+                            for pop in population_keys:
+                                density_profiles[pop][ind_Rm, ind_U, :] = state[pop]
 
-        # save data
-        save_mat_dict = {}
-        save_mat_dict['Rm_list'] = Rm_list
-        save_mat_dict['U_list'] = U_list
-        save_mat_dict['flux_mat'] = flux_mat
-        for pop in population_keys:
-            save_mat_dict[pop] = density_profiles[pop]
+                        except:
+                            print('failed to load', state_file)
+                    else:
+                        print(state_file, 'doesnt exist.')
 
-        compiled_set_name = 'compiled_'
-        compiled_set_name += 'N_' + str(num_cells)
-        compiled_set_name += '_mode_' + str(mode)
-        compiled_set_name += '_' + gas_name
-        compiled_save_file = main_folder + '/' + compiled_set_name + '.mat'
-        print('saving', compiled_save_file)
-        savemat(compiled_save_file, save_mat_dict)
+            # save data
+            save_mat_dict = {}
+            save_mat_dict['Rm_list'] = Rm_list
+            save_mat_dict['U_list'] = U_list
+            save_mat_dict['flux_mat'] = flux_mat
+            for pop in population_keys:
+                save_mat_dict[pop] = density_profiles[pop]
+
+            compiled_set_name = 'compiled_'
+            compiled_set_name += 'N_' + str(num_cells)
+            compiled_set_name += '_mode_' + str(mode)
+            compiled_set_name += f'_ft_{ft:g}'
+            compiled_save_file = main_folder + '/' + compiled_set_name + '.mat'
+            print('saving', compiled_save_file)
+            savemat(compiled_save_file, save_mat_dict)
--- run_scripts/run_slurm_parametric_MMM_v2.py
+++ run_scripts/run_slurm_parametric_MMM_v2.py
@@ -27,7 +27,8 @@ slurm_kwargs['ntasks'] = 1
 slurm_kwargs['cpus-per-task'] = 1
 
 # gas_name = 'deuterium'
-gas_name = 'tritium'
+# gas_name = 'tritium'
+gas_name = 'DT-mix'
 
 num_cells_list = [30]
 # num_cells_list = [10, 20, 30, 40, 50, 60, 70, 80]
@@ -36,8 +37,12 @@ modes = []
 modes += ['mahmir']
 modes += ['mekel']
 
+ft_list = []
+ft_list += [1]
+ft_list += [2]
+
 Rm_list = np.arange(2, 10.25, 0.25)
-U_list = np.arange(0, 1.05, 0.05)
+U_list = np.arange(0, 0.95, 0.05)
 
 total_number_of_sets = len(num_cells_list) * len(modes) * len(Rm_list) * len(U_list)
 print('total_number_of_sets = ' + str(total_number_of_sets))
@@ -45,74 +50,78 @@ cnt_sets = 0
 
 for num_cells in num_cells_list:
     for mode in modes:
-        for Rm in Rm_list:
-            for U in U_list:
+        for ft in ft_list:
+            for Rm in Rm_list:
+                for U in U_list:
 
-                cnt_sets += 1
+                    cnt_sets += 1
 
-                set_name = 'N_' + str(num_cells)
-                set_name += '_mode_' + str(mode)
-                set_name += f'_Rm_{Rm:g}'
-                set_name += f'_U_{U:g}'
-                set_name += '_' + gas_name
-                # print(set_name)
+                    set_name = 'N_' + str(num_cells)
+                    set_name += '_mode_' + str(mode)
+                    set_name += f'_ft_{ft:g}'
+                    set_name += f'_Rm_{Rm:g}'
+                    set_name += f'_U_{U:g}'
+                    set_name += '_' + gas_name
+                    # print(set_name)
 
-                print('@@@@@@@ set num', cnt_sets, '/', total_number_of_sets, ':', set_name)
+                    print('@@@@@@@ set num', cnt_sets, '/', total_number_of_sets, ':', set_name)
 
-                settings = {}
-                settings['gas_name'] = gas_name
+                    settings = {}
+                    settings['gas_name'] = gas_name
 
-                settings = define_default_settings(settings)
-                settings['draw_plots'] = False  # plotting not possible on slurm computers without display
+                    settings = define_default_settings(settings)
+                    settings['draw_plots'] = False  # plotting not possible on slurm computers without display
 
-                settings['n0'] = n0
-                settings['Ti_0'] = Ti
-                settings['Te_0'] = Ti
+                    settings['n0'] = n0
+                    settings['Ti_0'] = Ti
+                    settings['Te_0'] = Ti
 
-                settings['cell_size'] = 1.0  # m
+                    settings['cell_size'] = 1.0  # m
 
-                # MMM
-                settings['U0'] = U
-                if mode == 'old':
-                    settings['alpha_definition'] = 'geometric_constant_U0'
-                    settings['mmm_tL_transmission_factor'], settings['mmm_tR_transmission_factor'] = 0, 0
-                elif mode == 'mahmir':
-                    settings['alpha_definition'] = 'geometric_constant_U0'
-                    settings['mmm_tL_transmission_factor'], settings['mmm_tR_transmission_factor'] = 1, 0
-                elif mode == 'mekel':
-                    settings['alpha_definition'] = 'geometric_constant'
-                    settings['mmm_tL_transmission_factor'], settings['mmm_tR_transmission_factor'] = 1, 1
-                else:
-                    raise ValueError(f'invalid option for mode={mode}')
+                    settings['transmission_factor'] = ft
 
-                # mfp
-                settings['assume_constant_density'] = True
-                settings['assume_constant_temperature'] = True
-                settings['ion_scattering_rate_factor'] = 1800
-                settings['energy_conservation_scheme'] = 'none'
+                    # MMM
+                    settings['U0'] = U
+                    if mode == 'old':
+                        settings['alpha_definition'] = 'geometric_constant_U0'
+                        settings['mmm_tL_transmission_factor'], settings['mmm_tR_transmission_factor'] = 0, 0
+                    elif mode == 'mahmir':
+                        settings['alpha_definition'] = 'geometric_constant_U0'
+                        settings['mmm_tL_transmission_factor'], settings['mmm_tR_transmission_factor'] = 1, 0
+                    elif mode == 'mekel':
+                        settings['alpha_definition'] = 'geometric_constant'
+                        settings['mmm_tL_transmission_factor'], settings['mmm_tR_transmission_factor'] = 1, 1
+                    else:
+                        raise ValueError(f'invalid option for mode={mode}')
 
-                # settings['flux_normalized_termination_cutoff'] = 0.05
-                # settings['flux_normalized_termination_cutoff'] = 1e-2
-                settings['flux_normalized_termination_cutoff'] = 1e-3
-                # settings['flux_normalized_termination_cutoff'] = 1e-4
+                    # mfp
+                    settings['assume_constant_density'] = True
+                    settings['assume_constant_temperature'] = True
+                    settings['ion_scattering_rate_factor'] = 1800
+                    settings['energy_conservation_scheme'] = 'none'
 
-                settings['right_boundary_condition'] = 'none'
+                    # settings['flux_normalized_termination_cutoff'] = 0.05
+                    # settings['flux_normalized_termination_cutoff'] = 1e-2
+                    settings['flux_normalized_termination_cutoff'] = 1e-3
+                    # settings['flux_normalized_termination_cutoff'] = 1e-4
 
-                settings['number_of_cells'] = num_cells
+                    settings['right_boundary_condition'] = 'none'
 
-                # settings['transition_type'] = 'smooth_transition_to_free_flow'
-                settings['transition_type'] = 'none'
+                    settings['number_of_cells'] = num_cells
 
-                settings['Rm'] = Rm
+                    # settings['transition_type'] = 'smooth_transition_to_free_flow'
+                    settings['transition_type'] = 'none'
 
-                settings['save_dir'] = main_folder + '/' + set_name
-                state_file = settings['save_dir'] + '/state.pickle'
-                if os.path.exists(state_file):
-                    print('exists, skipping.')
-                else:
-                    os.makedirs(settings['save_dir'], exist_ok=True)
-                    os.chdir(settings['save_dir'])
+                    settings['Rm'] = Rm
 
-                    command = rate_eqs_script + ' --settings "' + str(settings) + '"'
-                    s = Slurm(set_name, slurm_kwargs=slurm_kwargs)
-                    s.run(command)
+                    settings['save_dir'] = main_folder + '/' + set_name
+                    state_file = settings['save_dir'] + '/state.pickle'
+                    if os.path.exists(state_file):
+                        print('exists, skipping.')
+                    else:
+                        os.makedirs(settings['save_dir'], exist_ok=True)
+                        os.chdir(settings['save_dir'])
+
+                        command = rate_eqs_script + ' --settings "' + str(settings) + '"'
+                        s = Slurm(set_name, slurm_kwargs=slurm_kwargs)
+                        s.run(command)
